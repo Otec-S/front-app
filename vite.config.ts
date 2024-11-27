c41baf87a9b99c4e83bfcf6ace49f466931c8b47
Sergey Grigorash <sergey.adviser@gmail.com>
2024-11-27 13:01:12 +0700

    feat Initial project setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import svgr from "vite-plugin-svgr";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    svgr({
+      include: "**/*.svg",
+    }),
+    react(),
+  ],
+  base: "MyPersonalSite/",
+  resolve: {
+    alias: {
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@shared": path.resolve(__dirname, "src/shared"),
+    },
+  },
+});
