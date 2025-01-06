27f080fdf3bc4646ae2494d93a0e8d2826f63529
Sergey Grigorash <sergey.adviser@gmail.com>
2025-01-06 11:13:59 +0500

    chore

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,8 +1,8 @@
 export const BASE_URL = "http://transferbox.ddns.net/api";
 
 export const ROUTES = {
-  HOME: "/front-app/",
-  SIGN_IN: "/front-app/sign-in",
-  SIGN_UP: "/front-app/sign-up",
-  TEST: "/front-app/test",
+  HOME: "/",
+  SIGN_IN: "/sign-in",
+  SIGN_UP: "/sign-up",
+  TEST: "/test",
 };
